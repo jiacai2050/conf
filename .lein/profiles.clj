0129ac9ea098c86133f46c62cc9f6ff2e49c5b53
jiacai2050 <jiacai2050@gmail.com>
2019-03-27 00:24:01 +0800

    add ultra plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,12 +1,14 @@
 {:user {:plugins [[lein-try "0.4.3"]
                   [com.gfredericks/lein-shorthand "0.4.1"]
-                  ]
-        :dependencies [[alembic "0.3.2"]
-                       ]
+                  [venantius/ultra "0.5.4"]]
+        :dependencies [[alembic "0.3.2"]]
         :shorthand {. {pp clojure.pprint/pprint
                        distill alembic.still/distill
+                       doc clojure.repl/doc
+                       source clojure.repl/source
                        lein alembic.still/lein
-                       join clojure.string/join}
-                    }
-;;        :repositories [["clojars" {:url "http://mirrors.ustc.edu.cn/clojars"}]]
-               }}
+                       javadoc clojure.java.javadoc/javadoc
+                       join clojure.string/join}}
+        ;; :ultra {:repl false}
+        ;; :repositories [["clojars" {:url "https://mirrors.ustc.edu.cn/clojars"}]]
+        }}
