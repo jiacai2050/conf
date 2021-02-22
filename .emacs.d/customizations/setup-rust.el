b5696f70da29aeac282bcc58f09f17f922f336f2
jiacai2050 <jiacai2050@gmail.com>
2021-02-22 08:49:48 +0800

    enable-rust-backtrack

--- .emacs.d/customizations/setup-rust.el
+++ .emacs.d/customizations/setup-rust.el
@@ -14,9 +14,15 @@
 
 (use-package cargo
   :hook ((rust-mode . cargo-minor-mode))
+  :config
+  (defun my/cargo-test-current ()
+    (interactive)
+    (setenv "RUST_LOG" "debug")
+    (cargo-process-current-test))
   :bind (:map rust-mode-map
-              (("C-c C-t" . cargo-process-current-test)))
+              (("C-c C-t" . my/cargo-test-current)))
   :custom ((cargo-process--command-current-test "test --color never")
+           (cargo-process--enable-rust-backtrace t)
            (cargo-process--command-flags "--  --nocapture")))
 
 (provide 'setup-rust)
