6363cf9ce87ce5a96dc4d659fdfac31c6c3d1b91
jiacai2050 <dev@liujiacai.net>
2021-03-06 14:00:00 +0800

    put org-mime-htmlize in message-send-hook

--- .config/offlineimap/pass.py
+++ .config/offlineimap/pass.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 import re
 import os
-import os.path
 
 
 def get_password_emacs(machine, login, port):
@@ -10,25 +9,3 @@ def get_password_emacs(machine, login, port):
     p = re.compile(s)
     authinfo = os.popen("gpg -q --no-tty -d ~/.config/authinfo.gpg").read()
     return p.search(authinfo).group(1)
-
-
-prioritized = ['INBOX', 'Drafts', 'Junk', 'Sent', 'Sent Messages']
-
-
-def mycmp(x, y):
-    for prefix in prioritized:
-        xsw = x.startswith(prefix)
-        ysw = y.startswith(prefix)
-        if xsw and ysw:
-            return cmp(x, y)
-        elif xsw:
-            return -1
-        elif ysw:
-            return +1
-    return cmp(x, y)
-
-
-def test_mycmp():
-    folders = os.listdir(os.path.expanduser('~/.mail/ljc'))
-    folders.sort(mycmp)
-    print folders
